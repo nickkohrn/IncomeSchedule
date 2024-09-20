07cc48a0d855df802250194dea42856212613062
Nick Kohrn <bnkohrn@icloud.com>
2024-09-20 12:56:47 -0400

    Add project files

---
+++ IncomeSchedule/IncomeScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  IncomeScheduleApp.swift
+//  IncomeSchedule
+//
+//  Created by Nick Kohrn on 9/20/24.
+//
+
+import SwiftUI
+
+@main
+struct IncomeScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IncomeScheduleTests/IncomeScheduleTests.swift
@@ -0,0 +1,17 @@
+//
+//  IncomeScheduleTests.swift
+//  IncomeScheduleTests
+//
+//  Created by Nick Kohrn on 9/20/24.
+//
+
+import Testing
+@testable import IncomeSchedule
+
+struct IncomeScheduleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
